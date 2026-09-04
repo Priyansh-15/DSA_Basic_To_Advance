b97d7c64a36255bfb7b51cb1988101bd1b495d4e
Priyansh Sharma <priyansh-p.sharma@thalesgroup.com>
2026-09-04 13:25:00 +0530

    basic cpp added

---
+++ basic.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    cout<<"Hi Priyansh!!"<<endl;
+    cout<<"The sum of "<<a<<" and "<<b<<" is: "<<a+b<<endl;
+    return 0;
+}
